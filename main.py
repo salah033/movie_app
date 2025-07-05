5ed5ae8377c4c11d24f043f9e242dd7843ba2c5f
salah033 <73083432+salah033@users.noreply.github.com>
2025-07-05 03:11:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ import pandas as pd
 import sqlite3
 from utils import reload_csv_to_sqlite
 
-# Call only once
-reload_csv_to_sqlite(
-    csv_path="TMDB_all_movies.csv",
-    db_path="movies.db"
-)
-
-
 pg = st.navigation([st.Page("Search.py"), 
                     st.Page("All_Movies.py"), 
                     st.Page("Details.py"),
